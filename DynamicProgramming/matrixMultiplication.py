c5bf2ce17a39624932252fbd35b15c418c7f9fd8
Anmol Pathak <pathakanmol90@gmail.com>
2023-08-19 00:15:23 +0530

    matrix multiplication solution

--- DynamicProgramming/matrixMultiplication.py
+++ DynamicProgramming/matrixMultiplication.py
@@ -1,17 +1,23 @@
+import sys
+
+
 class matrixMultiplication:
 
     def bottomUp(self,dim,num):
 
-        mem = [[-999 for x in range(num)] for y in range(num)]
+        mem = [[0 for x in range(num+1)] for y in range(num + 1)]
 
-        for i in range(num):
-            mem[i][i] = 0
+        print(mem)
 
-        for grp in range(2,num+1):
-            for i in range(1,num):
+        for grp in range(2,num + 1):
+            # print("value of grp is",grp)
+            #previously the for loop was not looping correctly (for i in range(1,num - grp + 1))
+            #so when i was not iterating for last value of loop .Hence an extra 1 is added here.
+            for i in range(1,num - grp + 2):
                 j = i + grp - 1
+                mem[i][j] = sys.maxsize
                 for k in range(i,j):
-                    q = mem[i][k] + mem[k+1][j] + (dim[k][1] * dim[j][0] * dim[j][1])
+                    q = mem[i][k] + mem[k+1][j] + (dim[i-1] * dim[k] * dim[j])
                     if q < mem[i][j]:
                         mem[i][j] = q
         return mem
@@ -22,7 +28,7 @@ class matrixMultiplication:
 if __name__ == '__main__':
 
     num = 3
-    dim = [[10,100],[100,5],[5,50]]
+    dim = [10,100,5,50]
 
     obj = matrixMultiplication()
     ans = obj.bottomUp(dim,num)
